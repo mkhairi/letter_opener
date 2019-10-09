30dc80f3cfe349ca89c38ba75e283e8771428767
Mohd Khairi <khairi@labs.my>
2019-10-09 09:22:35 +0800

    windows configuration

--- lib/letter_opener/configuration.rb
+++ lib/letter_opener/configuration.rb
@@ -1,10 +1,11 @@
 module LetterOpener
   class Configuration
-    attr_accessor :location, :message_template
+    attr_accessor :location, :message_template, :windows
 
     def initialize
       @location = Rails.root.join('tmp', 'letter_opener') if defined?(Rails) && Rails.respond_to?(:root)
       @message_template = 'default'
+      @windows = false
     end
   end
 end
